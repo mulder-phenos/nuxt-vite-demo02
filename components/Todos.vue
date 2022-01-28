f4b212644fe6aef76e29cd32f5f12a03174f9eb8
phenos-mulder <mulder@phenomx.ai>
2022-01-28 13:32:15 +0900

    it is first

---
+++ components/Todos.vue
@@ -0,0 +1,27 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+      <h1> This is Todo Mulder </h1>
+      <h1> This is Todo Mulder </h1>
+      <div> {{ data }} </div>
+      <div>
+        <a class="btn btn-primary" @click="sendOne()">Send</a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MulderTodos',
+  props:["data"],
+  created() {
+    console.log('todo - created')
+  },
+  methods: {
+    sendOne() {
+      console.log('toto - click - sendOne')
+    },
+  }
+  
+}
+</script>
